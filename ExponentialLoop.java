0f7eb1bc682d5d08473c4aa7e898da5c54540158
vvreddy8055 <bhimavarapu.vishnuvr8055@gmail.Com>
2025-05-16 07:47:12 +0530

    Add files via upload

---
+++ ExponentialLoop.java
@@ -0,0 +1,8 @@
+public class ExponentialLoop {
+    public static void main(String[] args) {
+        int n= 10;
+        for(int i=1;i<n;i*=2){
+            System.err.println(i);
+        }
+    }
+}
